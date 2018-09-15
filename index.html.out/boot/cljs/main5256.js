fe328d54d65da6606db9f811b103058b84388a5a
David Meister <thedavidmeister@gmail.com>
2018-09-16 00:36:53 +1000

    added nearseed CNAME

--- index.html.out/boot/cljs/main5256.js
+++ index.html.out/boot/cljs/main5256.js
@@ -1,5 +1,5 @@
 // Compiled by ClojureScript 1.10.339 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main4795');
+goog.provide('boot.cljs.main5256');
 goog.require('cljs.core');
 goog.require('cljs.core.constants');
 goog.require('pages.index');
